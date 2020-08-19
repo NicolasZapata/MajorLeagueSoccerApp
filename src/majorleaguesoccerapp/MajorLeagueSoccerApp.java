940495bc4cd50521496dc18daab65c69e661ce8f
NicolasZapata <35777697+NicolasZapata@users.noreply.github.com>
2020-08-19 13:30:59 -0500

    Initialize MLS App Platform for the UNIR's Course Advance Programing

---
+++ src/majorleaguesoccerapp/MajorLeagueSoccerApp.java
@@ -0,0 +1,16 @@
+package majorleaguesoccerapp;
+
+/**
+ *
+ * @author NICOLÁS ZAPATA ÁLZATE
+ */
+public class MajorLeagueSoccerApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
